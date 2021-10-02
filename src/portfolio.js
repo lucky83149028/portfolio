34f2fcb930d1a6b08b8ddadbe14adb6cae758320
lucky83149028 <zhengyun50511@gmail.com>
2021-10-03 02:21:57 +0800

    Work experience added

--- src/portfolio.js
+++ src/portfolio.js
@@ -93,24 +93,16 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      schoolName: "Dalian University of Technology",
+      logo: require("./assets/images/DUT.jpg"),
+      subHeader: "Computer science and Technology",
+      duration: "September 2007 - October 2011",
+      desc: "Bachelor's degree",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        // "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        // "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       ]
     },
-    {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
-    }
   ]
 };
 
@@ -120,17 +112,21 @@ const techStack = {
   viewSkillBars: true, //Set it to true to show Proficiency Section
   experience: [
     {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      Stack: "Solidity", //Insert stack or technology you have experience in
       progressPercentage: "90%" //Insert relative proficiency in percentage
     },
     {
-      Stack: "Backend",
+      Stack: "Web3.js",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "Node.js",
       progressPercentage: "70%"
     },
     {
-      Stack: "Programming",
-      progressPercentage: "60%"
-    }
+      Stack: "Go",
+      progressPercentage: "80%"
+    },
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
 };
@@ -141,30 +137,32 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      role: "Solidity developer",
+      company: "DAOventures",
+      companylogo: require("./assets/images/DAOventures.jpg"),
+      date: "April 2021 – Present",
+      desc: "DAOventures is a Multichain DeFi fund manager with borrowing and lending for decentralized finance (DeFi).",
+    },
+    {
+      role: "Blockchain & Full Stack developer",
+      company: "SonicX Blockchain",
+      companylogo: require("./assets/images/SonicX.png"),
+      date: "October 2019 – Jun 2020",
+      desc: "SonicX is a blockchain based on fork of TRON. I developed and managed chain nodes. My friends and me developed a DEX running on SonicX.",
+    },
+    {
+      role: "Blockchain & Full Stack developer",
+      company: "Freelance",
+      companylogo: require("./assets/images/Freelance.jpg"),
+      date: "December 2011 – March 2021",
+      desc: "My first step in Blockchain industry was to develop a hybrid mobile wallet.",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Mobile and Web wallet",
+        "Centralized Exchanges",
+        "Web scraping",
+        "Network programming",
       ]
     },
-    {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    }
   ]
 };
 
